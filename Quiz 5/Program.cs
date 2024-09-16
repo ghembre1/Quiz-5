2c25f8f17ca8233411fa4c67dff57b8e54d737fd
Gary Hembree <gbhembree22@amarillocollege.com>
2024-09-16 13:01:25 -0500

    Add project files.

---
+++ Quiz 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Quiz_5
+{
+    internal class Program
+    {
+        static void CheckDuplicate(string[] args) // method for checkduplicate
+        { 
+
+            for (int i = 0; i < args.Length; i++) // goes through every letter in the string
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: "); // prompts user to enter a string
+            string word = Console.ReadLine(); // takes users input
+
+            CheckDuplicate(word);
+            
+
+
+
+
+
+            Console.WriteLine("Result: "); // prompts the user with the results
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Quiz 5/ProgramHelpers.cs
@@ -0,0 +1,11 @@
+﻿internal static class ProgramHelpers
+{
+    static void CheckDuplicate(string)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            char c = (char)i;
+        }
+        return;
+    }
+}
